2080ff7a80ddbd2b634b3c21bb59a8dcc4bc60b6
Christian Haudum <christian.haudum@gmail.com>
2024-06-27 16:33:59 +0200

    Extend bloom-inspector tool
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- tools/bloom/inspector/main.go
+++ tools/bloom/inspector/main.go
@@ -10,12 +10,15 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run main.go BLOCK_DIRECTORY")
+		fmt.Println("Usage: go run main.go BLOCK_DIRECTORY [TEST ...]")
 		os.Exit(2)
 	}
 
 	path := os.Args[1]
 	fmt.Printf("Block directory: %s\n", path)
+
+	test := os.Args[2:]
+	fmt.Printf("Test: %v\n", test)
 
 	r := v1.NewDirectoryBlockReader(path)
 	b := v1.NewBlock(r, v1.NewMetrics(nil))
@@ -28,9 +31,23 @@ func main() {
 
 	fmt.Printf("Metadata: %+v\n", md)
 
-	for q.Next() {
-		swb := q.At()
-		fmt.Printf("%s (%d)\n", swb.Series.Fingerprint, swb.Series.Chunks.Len())
+	itr := q.Iter()
+	for itr.Next() {
+		q := itr.At()
+		if len(test) > 0 {
+			for q.Blooms.Next() {
+				bloom := q.Blooms.At()
+				for i := range test {
+					tokenizer := v1.NewNGramTokenizer(md.Options.Schema.NGramLen(), md.Options.Schema.NGramSkip())
+					tokens := tokenizer.Tokens(test[i])
+					for tokens.Next() {
+						res := bloom.Test(tokens.At())
+						fmt.Printf("%s => %v\n", tokens.At(), res)
+					}
+				}
+			}
+		}
+		fmt.Printf("%s (%d)\n", q.Series.Fingerprint, q.Series.Chunks.Len())
 	}
 	if q.Err() != nil {
 		fmt.Printf("error: %s\n", q.Err())
